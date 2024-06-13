732a87da6603d6053ff9d5d3ec447a48ad82549e
Woker21 <112359676+woker21@users.noreply.github.com>
2024-06-13 14:11:24 +0200

    sddsdsd

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -23,7 +23,16 @@ export default function TabLayout() {
           ),
         }}
       />
-
+      <Tabs.Screen
+        name="home"
+        
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
